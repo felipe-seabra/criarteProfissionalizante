242c56fb46b9f282c6af4fdf9aaeff73bc670e9c
Felipe Seabra <feliperoce@gmail.com>
2022-07-01 14:03:26 -0300

    Inserindo janela de cokies

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="style.css" />
+    <style>
+      .cookieConsentContainer {
+        z-index: 999;
+        width: 350px;
+        min-height: 20px;
+        box-sizing: border-box;
+        padding: 30px 30px 30px 30px;
+        background: #0d1b2a;
+        overflow: hidden;
+        position: fixed;
+        bottom: 30px;
+        right: 30px;
+        display: none;
+      }
+      .cookieConsentContainer .cookieTitle a {
+        font-family: OpenSans, arial, sans-serif;
+        color: #fff;
+        font-size: 22px;
+        line-height: 20px;
+        display: block;
+      }
+      .cookieConsentContainer .cookieDesc p {
+        margin: 0;
+        padding: 0;
+        font-family: OpenSans, arial, sans-serif;
+        color: #fff;
+        font-size: 13px;
+        line-height: 20px;
+        display: block;
+        margin-top: 10px;
+      }
+      .cookieConsentContainer .cookieDesc a {
+        font-family: OpenSans, arial, sans-serif;
+        color: #fff;
+        text-decoration: underline;
+      }
+      .cookieConsentContainer .cookieButton a {
+        display: inline-block;
+        font-family: OpenSans, arial, sans-serif;
+        color: #fff;
+        font-size: 14px;
+        font-weight: 700;
+        margin-top: 14px;
+        background: #415a77;
+        border-radius: 5px;
+        box-sizing: border-box;
+        padding: 15px 24px;
+        text-align: center;
+        transition: background 0.3s;
+      }
+      .cookieConsentContainer .cookieButton a:hover {
+        cursor: pointer;
+        background: #49698d;
+      }
+      @media (max-width: 980px) {
+        .cookieConsentContainer {
+          bottom: 0 !important;
+          left: 0 !important;
+          width: 100% !important;
+        }
+      }
+    </style>
+
+    <title>Colégio Criarte - Cursos Profissionalizantes</title>
+  </head>
+  <body>
+    <header>
+      <nav class="header">
+        <div class="name-logo active">
+          <a href="#"><img class="logo" src="images/logo-white.png" alt="Logo Colégio Criarte" /></a>
+        </div>
+        <div class="nav-links">
+          <h1>Cursos <span>Profissionalizantes</span></h1>
+          <!-- <ul>
+            <li><a href="#"> Home </a> &nbsp; &nbsp;</li>
+            <li><a href="about.html"> Sobre </a> &nbsp; &nbsp;</li>
+            <li><a href="projects.html"> Projetos </a></li>
+          </ul> -->
+        </div>
+        <div class="active sociais">
+          <a href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767" target="_blank">
+            <img class="social" src="images/whatsapp-gradient.png" alt="Logo Whatsapp" />
+          </a>
+          &nbsp; &nbsp;
+          <a href="https://www.facebook.com/criarteprofissionalizante" target="_blank">
+            <img class="social" src="images/facebook.png" alt="Logo Facebook" />
+          </a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <article class="pt-4">
+        <section class="pt-4 container justify-center">
+          <div class="text-center mt-5">
+            <h2>Técnico em Enfermagem</h2>
+            <br />
+            <div class="container justify-center">
+              <picture>
+                <img class="tecs" src="images/enfermagem.png" alt="Foto de uma técnica em farmácia" />
+              </picture>
+              <div class="">
+                <p>
+                  O Curso Técnico em Farmácia tem como objetivo formar pessoal qualificado para atuar na área de vendas,
+                  dispensação de materiais, auxiliar na manipulação de medicamentos, além de atuar na distribuição de
+                  medicamentos da farmácia hospitalar. <br /><br />
+                  <strong>ÁREA DE ATUAÇÃO:</strong>
+                </p>
+                <ul class="list">
+                  <li>drogarias comerciais;</li>
+                  <li>farmácias de manipulação;</li>
+                  <li>farmácias hospitalares e de Unidades Básicas de Saúde;</li>
+                  <li>distribuidoras de medicamentos, insumos e correlatos;</li>
+                  <li>indústrias farmacêuticas.</li>
+                </ul>
+                <p>
+                  <em>Suas atividades são desenvolvidas sob supervisão e orientação do Farmacêutico.</em> <br />
+                  <br /><strong> DURAÇÃO: </strong> 19 meses
+                </p>
+              </div>
+            </div>
+            <br /><br />
+            <a
+              href="http://www.colegiocriartepp.com.br/formulario/"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              >Quero me inscrever</a
+            >
+            &nbsp; &nbsp;
+            <a
+              href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              id="green"
+              >Quero saber mais</a
+            >
+          </div>
+        </section>
+        <br />
+        <hr />
+        <section class="pt-4 container justify-center">
+          <div class="text-center">
+            <h2>Técnico em Farmácia</h2>
+            <br />
+            <div class="container justify-center">
+              <picture>
+                <img class="tecs" src="images/farmacia.png" alt="Foto de uma técnica em farmácia" />
+              </picture>
+              <div>
+                <p>
+                  O Curso Técnico em Farmácia tem como objetivo formar pessoal qualificado para atuar na área de vendas,
+                  dispensação de materiais, auxiliar na manipulação de medicamentos, além de atuar na distribuição de
+                  medicamentos da farmácia hospitalar. <br /><br />
+                  <strong>ÁREA DE ATUAÇÃO:</strong>
+                </p>
+                <ul class="list">
+                  <li>drogarias comerciais;</li>
+                  <li>farmácias de manipulação;</li>
+                  <li>farmácias hospitalares e de Unidades Básicas de Saúde;</li>
+                  <li>distribuidoras de medicamentos, insumos e correlatos;</li>
+                  <li>indústrias farmacêuticas.</li>
+                </ul>
+                <p>
+                  <em>Suas atividades são desenvolvidas sob supervisão e orientação do Farmacêutico.</em> <br />
+                  <br /><strong> DURAÇÃO: </strong> 19 meses
+                </p>
+              </div>
+            </div>
+            <br /><br />
+            <a
+              href="http://www.colegiocriartepp.com.br/formulario/"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              >Quero me inscrever</a
+            >
+            &nbsp; &nbsp;
+            <a
+              href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              id="green"
+              >Quero saber mais</a
+            >
+          </div>
+        </section>
+        <br />
+        <hr />
+        <section class="pt-4 container justify-center">
+          <div class="text-center">
+            <h2>Técnico em Radiologia</h2>
+            <br />
+            <div class="container justify-center">
+              <picture>
+                <img class="tecs" src="images/radiologia.png" alt="Foto de uma técnica em farmácia" />
+              </picture>
+              <div>
+                <p>
+                  O Curso Técnico em Radiologia prepara o profissional para atuar nas diversas áreas de aplicação da
+                  radiação ionizante e fornecer subsídios de imagem para o diagnóstico de diversas patologias.<br />
+                  O profissional realiza exames radiográficos convencionais. Processa filmes radiológicos. Prepara o
+                  paciente e o ambiente para a realização de exames nos serviços de radiologia e diagnóstico por imagem,
+                  tais como: mamografia, hemodinâmica, tomografia computadorizada, densitometria óssea, ressonância
+                  magnética nuclear e ultra-sonografia. Acompanha a utilização de meios de contraste radiológicos,
+                  observando os princípios de proteção radiológica, avaliando reações adversas e agindo em situações de
+                  urgência, sob supervisão profissional pertinente. <br /><br />
+                  <strong>ÁREA DE ATUAÇÃO:</strong>
+                </p>
+                <ul class="list">
+                  <li>hospitais;</li>
+                  <li>clínicas;</li>
+                  <li>prontos-socorros;</li>
+                  <li>laboratórios de radiologia.</li>
+                </ul>
+                <p><br /><strong> DURAÇÃO: </strong> 18 meses</p>
+              </div>
+            </div>
+            <br /><br />
+            <a
+              href="http://www.colegiocriartepp.com.br/formulario/"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              >Quero me inscrever</a
+            >
+            &nbsp; &nbsp;
+            <a
+              href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
+              target="_blank"
+              alt="Link para formulário"
+              class="botton"
+              id="green"
+              >Quero saber mais</a
+            >
+          </div>
+        </section>
+        <br />
+        <hr />
+      </article>
+    </main>
+    <footer>
+      <div class="footer">
+        <p class="text-center">Colégio Criarte - &copy; 2022</p>
+      </div>
+    </footer>
+    <script>
+      var purecookieTitle = "Cookies",
+        purecookieDesc = "Este site utiliza cookies para seu adequado funcionamento, análises, personalização e publicidade. Acesse nossa",
+        purecookieLink = '<a href="https://www.colegiocriartepp.com.br/politica/" target="_blank">Política de Privacidade</a>',
+        purecookieButton = "Aceitar";
+      function pureFadeIn(e, o) {
+        var i = document.getElementById(e);
+        (i.style.opacity = 0),
+          (i.style.display = o || "block"),
+          (function e() {
+            var o = parseFloat(i.style.opacity);
+            (o += 0.02) > 1 || ((i.style.opacity = o), requestAnimationFrame(e));
+          })();
+      }
+      function pureFadeOut(e) {
+        var o = document.getElementById(e);
+        (o.style.opacity = 1),
+          (function e() {
+            (o.style.opacity -= 0.02) < 0 ? (o.style.display = "none") : requestAnimationFrame(e);
+          })();
+      }
+      function setCookie(e, o, i) {
+        var t = "";
+        if (i) {
+          var n = new Date();
+          n.setTime(n.getTime() + 24 * i * 60 * 60 * 1e3), (t = "; expires=" + n.toUTCString());
+        }
+        document.cookie = e + "=" + (o || "") + t + "; path=/";
+      }
+      function getCookie(e) {
+        for (var o = e + "=", i = document.cookie.split(";"), t = 0; t < i.length; t++) {
+          for (var n = i[t]; " " == n.charAt(0); ) n = n.substring(1, n.length);
+          if (0 == n.indexOf(o)) return n.substring(o.length, n.length);
+        }
+        return null;
+      }
+      function eraseCookie(e) {
+        document.cookie = e + "=; Max-Age=-99999999;";
+      }
+      function cookieConsent() {
+        getCookie("purecookieDismiss") ||
+          ((document.body.innerHTML +=
+            '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>' +
+            purecookieTitle +
+            '</a></div><div class="cookieDesc"><p>' +
+            purecookieDesc +
+            " " +
+            purecookieLink +
+            '</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">' +
+            purecookieButton +
+            "</a></div></div>"),
+          pureFadeIn("cookieConsentContainer"));
+      }
+      function purecookieDismiss() {
+        setCookie("purecookieDismiss", "1", 7), pureFadeOut("cookieConsentContainer");
+      }
+      window.onload = function () {
+        cookieConsent();
+      };
+    </script>
+
+    <script src="script.js"></script>
+  </body>
+</html>
