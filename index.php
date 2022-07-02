6dda2fa3d885a7b0d83978239dd51b1b6fcec0b9
Felipe Seabra <feliperoce@gmail.com>
2022-07-02 17:41:58 -0300

    Separando arquivos js e css e alinhando buttons

--- index.php
+++ index.php
@@ -6,68 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
-    <style>
-      .cookieConsentContainer {
-        z-index: 999;
-        width: 350px;
-        min-height: 20px;
-        box-sizing: border-box;
-        padding: 30px 30px 30px 30px;
-        background: #0d1b2a;
-        overflow: hidden;
-        position: fixed;
-        bottom: 30px;
-        right: 30px;
-        display: none;
-      }
-      .cookieConsentContainer .cookieTitle a {
-        font-family: OpenSans, arial, sans-serif;
-        color: #fff;
-        font-size: 22px;
-        line-height: 20px;
-        display: block;
-      }
-      .cookieConsentContainer .cookieDesc p {
-        margin: 0;
-        padding: 0;
-        font-family: OpenSans, arial, sans-serif;
-        color: #fff;
-        font-size: 13px;
-        line-height: 20px;
-        display: block;
-        margin-top: 10px;
-      }
-      .cookieConsentContainer .cookieDesc a {
-        font-family: OpenSans, arial, sans-serif;
-        color: #fff;
-        text-decoration: underline;
-      }
-      .cookieConsentContainer .cookieButton a {
-        display: inline-block;
-        font-family: OpenSans, arial, sans-serif;
-        color: #fff;
-        font-size: 14px;
-        font-weight: 700;
-        margin-top: 14px;
-        background: #415a77;
-        border-radius: 5px;
-        box-sizing: border-box;
-        padding: 15px 24px;
-        text-align: center;
-        transition: background 0.3s;
-      }
-      .cookieConsentContainer .cookieButton a:hover {
-        cursor: pointer;
-        background: #49698d;
-      }
-      @media (max-width: 980px) {
-        .cookieConsentContainer {
-          bottom: 0 !important;
-          left: 0 !important;
-          width: 100% !important;
-        }
-      }
-    </style>
 
     <title>Colégio Criarte - Cursos Profissionalizantes</title>
   </head>
@@ -134,14 +72,13 @@
               class="botton"
               >Quero me inscrever</a
             >
-            &nbsp; &nbsp;
             <a
               href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
               target="_blank"
               alt="Link para formulário"
               class="botton"
               id="green"
-              >Quero saber mais</a
+              >Saber mais</a
             >
           </div>
         </section>
@@ -183,14 +120,13 @@
               class="botton"
               >Quero me inscrever</a
             >
-            &nbsp; &nbsp;
             <a
               href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
               target="_blank"
               alt="Link para formulário"
               class="botton"
               id="green"
-              >Quero saber mais</a
+              >Saber mais</a
             >
           </div>
         </section>
@@ -233,14 +169,13 @@
               class="botton"
               >Quero me inscrever</a
             >
-            &nbsp; &nbsp;
             <a
               href="https://api.whatsapp.com/send?text=Ol%C3%A1!&phone=+5518981029767"
               target="_blank"
               alt="Link para formulário"
               class="botton"
               id="green"
-              >Quero saber mais</a
+              >Saber mais</a
             >
           </div>
         </section>
@@ -253,67 +188,6 @@
         <p class="text-center">Colégio Criarte - &copy; 2022</p>
       </div>
     </footer>
-    <script>
-      var purecookieTitle = "Cookies",
-        purecookieDesc = "Este site utiliza cookies para seu adequado funcionamento, análises, personalização e publicidade. Acesse nossa",
-        purecookieLink = '<a href="https://www.colegiocriartepp.com.br/politica/" target="_blank">Política de Privacidade</a>',
-        purecookieButton = "Aceitar";
-      function pureFadeIn(e, o) {
-        var i = document.getElementById(e);
-        (i.style.opacity = 0),
-          (i.style.display = o || "block"),
-          (function e() {
-            var o = parseFloat(i.style.opacity);
-            (o += 0.02) > 1 || ((i.style.opacity = o), requestAnimationFrame(e));
-          })();
-      }
-      function pureFadeOut(e) {
-        var o = document.getElementById(e);
-        (o.style.opacity = 1),
-          (function e() {
-            (o.style.opacity -= 0.02) < 0 ? (o.style.display = "none") : requestAnimationFrame(e);
-          })();
-      }
-      function setCookie(e, o, i) {
-        var t = "";
-        if (i) {
-          var n = new Date();
-          n.setTime(n.getTime() + 24 * i * 60 * 60 * 1e3), (t = "; expires=" + n.toUTCString());
-        }
-        document.cookie = e + "=" + (o || "") + t + "; path=/";
-      }
-      function getCookie(e) {
-        for (var o = e + "=", i = document.cookie.split(";"), t = 0; t < i.length; t++) {
-          for (var n = i[t]; " " == n.charAt(0); ) n = n.substring(1, n.length);
-          if (0 == n.indexOf(o)) return n.substring(o.length, n.length);
-        }
-        return null;
-      }
-      function eraseCookie(e) {
-        document.cookie = e + "=; Max-Age=-99999999;";
-      }
-      function cookieConsent() {
-        getCookie("purecookieDismiss") ||
-          ((document.body.innerHTML +=
-            '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>' +
-            purecookieTitle +
-            '</a></div><div class="cookieDesc"><p>' +
-            purecookieDesc +
-            " " +
-            purecookieLink +
-            '</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">' +
-            purecookieButton +
-            "</a></div></div>"),
-          pureFadeIn("cookieConsentContainer"));
-      }
-      function purecookieDismiss() {
-        setCookie("purecookieDismiss", "1", 7), pureFadeOut("cookieConsentContainer");
-      }
-      window.onload = function () {
-        cookieConsent();
-      };
-    </script>
-
-    <script src="script.js"></script>
+    <script src="scriptCookie.js"></script>
   </body>
 </html>
